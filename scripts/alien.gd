a7c7a8f2ffdba2048288c40a77cfe4cb67851ee3
mochiballz <mochiballz@tachibana.navi>
2026-05-02 11:24:50 -0500

    Enlarged level_1, added Camera2D to alien w/ smoothing

--- scripts/alien.gd
+++ scripts/alien.gd
@@ -16,6 +16,8 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		$AnimatedSprite2D.animation = "jump"
+		if velocity.y > 1:
+			$AnimatedSprite2D.animation = "dive"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
