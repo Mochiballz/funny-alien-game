0f657ebd57847e0af9277fa567e0d4f9fbb98a67
mochiballz <mochiballz@tachibana.navi>
2026-05-04 18:37:02 -0500

    Tweaked with dive animation(?)

--- scripts/alien.gd
+++ scripts/alien.gd
@@ -19,6 +19,7 @@ func _physics_process(delta: float) -> void:
 		if velocity.y > 1:
 			$AnimatedSprite2D.animation = "dive"
 
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
